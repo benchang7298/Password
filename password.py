08bf8ca7e2de0b320dbd7ae29de43254f5b88179
Ben Chang <ben.chang@intel.com>
2021-07-29 16:07:48 +0800

    Add files via upload

--- password.py
+++ password.py
@@ -2,15 +2,15 @@ password = 'a9825bon'
 count = 3
 count = int(count)
 while count > 0: 
+    count = count - 1  
     pwd = input('please enter password:')
     if pwd == password:
        print('you are right')
        break
-    else:
-           count = count - 1    
-           print('you have', count,'chance')
-        
-         
+    else:  
+           print('wrong password')
+           if count > 0:
+              print('you have', count,'chance')
          
 
                
